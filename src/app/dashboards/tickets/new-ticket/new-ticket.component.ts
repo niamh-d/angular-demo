9a83393a9dfe5f8db55d2939d0ea276ed894c6ec
Niamh <85398097+niamh-d@users.noreply.github.com>
2025-11-30 21:47:22 +0200

    shared control component

--- src/app/dashboards/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboards/tickets/new-ticket/new-ticket.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
+import { Control } from '../../../shared/control/control';
 
 @Component({
   selector: 'app-new-ticket',
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, Control],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css',
 })
