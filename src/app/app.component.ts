4cc6258d042db05eec6ab435c7b1390f772b5c92
Niamh <85398097+niamh-d@users.noreply.github.com>
2025-11-30 19:26:52 +0200

    reusable dashboard item component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,20 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { ServerStatusDash } from './dashboards/server-status-dash/server-status-dash';
 import { TrafficDash } from './dashboards/traffic-dash/traffic-dash';
-import { SupportTicketsDash } from './dashboards/support-tickets-dash/support-tickets-dash';
+import { SupportTicketsDash } from './dashboards/support-tickets-dash/support-tickets-dash.component';
+import { DashboardItemComponent } from './dashboards/dashboard-item/dashboard-item.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, ServerStatusDash, TrafficDash, SupportTicketsDash],
+  imports: [
+    HeaderComponent,
+    ServerStatusDash,
+    TrafficDash,
+    SupportTicketsDash,
+    DashboardItemComponent,
+  ],
 })
 export class AppComponent {
   dummyTrafficData = [
---
+++ src/app/dashboards/support-tickets-dash/support-tickets-dash.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-support-tickets-dash',
+  imports: [],
+  templateUrl: './support-tickets-dash.component.html',
+  styleUrl: './support-tickets-dash.component.css',
+})
+export class SupportTicketsDash {}
